4d1374636b708c1b0709c5860c8551acd51ee861
Bhaktiben Kadiya <56860402+Bhakti-Kadiya@users.noreply.github.com>
2021-03-16 14:32:12 +0530

    create react app

--- src/App.js
+++ src/App.js
@@ -3,33 +3,46 @@ import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
-import Player from './Player'
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const[{ user, _token}, dispatch] = useDataLayerValue();
+
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
+    
 
     if (_token) {
+
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      });
       setToken(_token);
       spotify.setAccessToken(_token);
 
       spotify.getMe().then((user) =>{
-        console.log("user",user);
-      } );
+        console.log('🤠',user);
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
     }
 
-    console.log("i have a token", token);
+    console.log("i have a token", _token);
   }, []);
 
   return (
-    <div className="App">
-      {token ? (<Player />)
-        : (<Login />)}
+    <div className="App">{token ? 
+    <Player />:
+    <Login />}
     </div>
   );
 }
